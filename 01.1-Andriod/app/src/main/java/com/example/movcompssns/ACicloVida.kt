2b8605c94ce5c391bc8eed8a4214ba2b27fec3c5
SergioNaranjoS <97323709+SergioNaranjoS@users.noreply.github.com>
2022-12-20 12:37:26 -0500

    Android1.1

---
+++ 01.1-Andriod/app/src/main/java/com/example/movcompssns/ACicloVida.kt
@@ -0,0 +1,78 @@
+package com.example.movcompssns
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.movcompssns.databinding.ActivityAcicloVidaBinding
+
+class ACicloVida : AppCompatActivity() {
+
+    var textoGlobal = ""
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityAcicloVidaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAcicloVidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        //mostrar
+        mostrarSnackbar("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("OnResume")
+    }
+    override fun onRestart() {
+        super.onResume()
+        mostrarSnackbar("OnRestart")
+    }
+    override fun onPause() {
+        super.onResume()
+        mostrarSnackbar("OnPause")
+    }
+    override fun onStop() {
+        super.onResume()
+        mostrarSnackbar("OnPause")
+    }
+    override fun onDestroy() {
+        super.onResume()
+        mostrarSnackbar("OnPause")
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    fun mostrarSnackbar(texto:String){
+        textoGlobal += texto
+        Snackbar.make(findViewById(R.id.cl_ciclo_vida),
+            textoGlobal, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+}
