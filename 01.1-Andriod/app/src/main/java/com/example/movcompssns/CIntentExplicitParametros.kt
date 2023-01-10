a90e4fc7feae3ec47e6b4fc3010dc4858932423a
SergioNaranjoS <97323709+SergioNaranjoS@users.noreply.github.com>
2023-01-10 12:52:59 -0500

    Intent v2
    Intent explicito e Implicito, implicito muestra la seleccion de contatos y el explicito recibe y devuelve datos

--- 01.1-Andriod/app/src/main/java/com/example/movcompssns/CIntentExplicitParametros.kt
+++ 01.1-Andriod/app/src/main/java/com/example/movcompssns/CIntentExplicitParametros.kt
@@ -1,11 +1,34 @@
 package com.example.movcompssns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CIntentExplicitParametros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cintent_explicit_parametros)
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+
+        //lamar a devolver respuesta
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton
+            .setOnClickListener{devolverRespuesta()}
+    }
+
+    fun devolverRespuesta(){
+        val intentDevolverParametros = Intent()
+        intentDevolverParametros.putExtra("nombreModificado", "Pedro")
+        intentDevolverParametros.putExtra("edadModificado", 32)
+        //this.resulte
+        setResult(
+            RESULT_OK,
+            intentDevolverParametros
+        )
+        //this.finish()
+        finish()
     }
 }
