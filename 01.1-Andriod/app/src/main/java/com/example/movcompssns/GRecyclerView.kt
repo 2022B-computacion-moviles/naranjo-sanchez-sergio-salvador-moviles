46f0a75f617709684f882b4cc5f520b3ccb82f01
SergioNaranjoS <97323709+SergioNaranjoS@users.noreply.github.com>
2023-01-30 11:59:52 -0500

    30-01-23
    Listas iterables, Inner class

--- 01.1-Andriod/app/src/main/java/com/example/movcompssns/GRecyclerView.kt
+++ 01.1-Andriod/app/src/main/java/com/example/movcompssns/GRecyclerView.kt
@@ -2,10 +2,33 @@ package com.example.movcompssns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class GRecyclerView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grecycler_view)
+
+        val listaEntrenador = arrayListOf<BEntrenador>()
+        listaEntrenador
+            .add(BEntrenador(1, "Juan", "Perez"))
+        listaEntrenador
+            .add(BEntrenador(2, "Alberto", "Anqyr"))
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_entrenadores)
+        inicializarRecyclerView(listaEntrenador, recyclerView)
+    }
+    fun inicializarRecyclerView(
+        lista: ArrayList<BEntrenador>,
+        recyclerView: RecyclerView
+    ){
+        val adaptador = FRecyclerViewAdaptadorNombreCedula(
+            this,
+            lista,
+            recyclerView
+        )
+        recyclerView.adapter = adaptador
+        recyclerView.itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        adaptador.notifyDataSetChanged()
     }
 }
