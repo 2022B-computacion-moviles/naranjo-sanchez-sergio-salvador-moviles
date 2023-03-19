1cf62c1b4f71d40cbeaba96913f0d3463404e26a
SergioNaranjoS <97323709+SergioNaranjoS@users.noreply.github.com>
2023-03-18 19:54:12 -0500

    Proyecto
    Contacto ScrollLayout

--- Proyecto/app/src/main/java/com/example/movcompssnspro/MainActivity.kt
+++ Proyecto/app/src/main/java/com/example/movcompssnspro/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.movcompssnspro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract.Instances
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botonContacto = findViewById<Button>(R.id.button_contactos)
+        botonContacto
+            irActividad(Contacto::class.java)
+    }
+    fun irActividad(
+        clase: Class<*>
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
+
     }
 }
